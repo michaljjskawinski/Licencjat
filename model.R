40a9f1f1c197f8e3899215021004f942e231b2eb
Michał <michaljjskawinski@wp.pl>
2020-11-02 17:38:44 +0100

    update + saving csv files

--- model.R
+++ model.R
@@ -16,18 +16,21 @@ diet <- read.csv2(file = paste("diet/",diet_type,".csv",sep =""), sep = ';', hea
 
 
 #WEJŚCIE 2: skład mikrobiomu (narazie jedna bakteria)
-bacteriaH <- c("Akkermansia_muciniphila_ATCC_BAA_835.mat",
-               "Bacteroides_sp_2_1_7.mat",
-               "Bacteroides_thetaiotaomicron_VPI_5482.mat",
-               "Bacteroides_vulgatus_ATCC_8482.mat",
-               "Bifidobacterium_mongoliense_DSM_21395.mat",
-               "Clostridium_sp_SS2_1.mat",
-               "Faecalibacterium_prausnitzii_SL3_3.mat",
-               "Lactobacillus_amylovorus_GRL_1112.mat",
-               "Roseburia_intestinalis_L1_82.mat",
-               "Ruminococcus_sp_SR1_5.mat")
-bacteria_ammountH <- c(10,10,10,10,10,10,10,10,10,10)
 
+#bacteriaH <- c("Akkermansia_muciniphila_ATCC_BAA_835.mat",
+#               "Bacteroides_sp_2_1_7.mat",
+#               "Bacteroides_thetaiotaomicron_VPI_5482.mat",
+#               "Bacteroides_vulgatus_ATCC_8482.mat",
+#               "Bifidobacterium_mongoliense_DSM_21395.mat",
+#               "Clostridium_sp_SS2_1.mat",
+#               "Faecalibacterium_prausnitzii_SL3_3.mat",
+#               "Lactobacillus_amylovorus_GRL_1112.mat",
+#               "Roseburia_intestinalis_L1_82.mat",
+#               "Ruminococcus_sp_SR1_5.mat")
+#bacteria_ammountH <- c(10,10,10,10,10,10,10,10,10,10)
+
+bacteriaH <- c("Akkermansia_muciniphila_ATCC_BAA_835.mat", "Bacteroides_sp_2_1_7.mat")
+bacteria_ammountH <- c(10,10)
 
 bacteriaUnH <- c("Bifidobacterium_mongoliense_DSM_21395.mat")
 bacteria_ammountUnH <- c(20)
@@ -40,34 +43,14 @@ if (microbiom == "healthy") {
   bacteria_ammount <- bacteria_ammountUnH
 }
 
-substances <- c()
-for(i in 1:nrow(diet)) {
-  substances <- append(substances, as.character(diet[i,]$Reaction))
-}
-bacteria_change <- data.frame("Bacteria" = bacteria, "Biomass_0" = bacteria_ammount, "Biomass_end" = bacteria_ammount) 
-for(s in substances) {
-  tmp <- data.frame(rep(FALSE,length(bacteria)))
-  colnames(tmp) = s
-  bacteria_change <- cbind(bacteria_change,tmp)
-}
-
-
-
-
-
 #STWORZENIE ARENY I DODANIE BAKTERII
 arena <- Arena(n=100,m=100)
 
 for (i in 1:length(bacteria)) {
   bac <- readMATmod(paste("bacteria/",bacteria[i],sep=""))
   arena <- addOrg(arena,Bac(bac),amount=bacteria_ammount[i])
-  #arena <- addEssentialMed(arena, Bac(bac))
+  arena <- addEssentialMed(arena, Bac(bac))  
   #arena <- addDeafultMed(arena, Bac(bac))
-  
-  for(j in Bac(bac)@medium) {
-    #bacteria_change[[which(colnames(bacteria_change) == j)]][i] <- TRUE
-    try(bacteria_change[[which(colnames(bacteria_change) == j)]][i] <- TRUE,silent = TRUE)
-  }
 }
 
 
@@ -84,29 +67,29 @@ for(i in 1:nrow(diet)) {
 sim_no <- 10
 simulation <- simEnv(arena,time=sim_no)
 #zapisywanie symulacji
-save(simulation,file = "simulationManyFatWithoutEssentials.RData")
+#save(simulation,file = "simulationManyOnlyEssentials.RData")
 #wczytywanie symulacji
 #load("simulation4.RData")
 
 
-for (i in 1:nrow(bacteria_change)) { #count czy biomasa ? początkowa i końcowa
-  biomass_end <- sum(simulation@simlist[[sim_no+1]]$biomass)
-  bacteria_change[i,]$Biomass_end <- biomass_end
-}
+#STWORZENIE CSV Z ILOŚCIĄ BAKTERII
+list <- lapply(simulation@simlist, function(x){
+  occ <- table(x$type)
+  unlist(lapply(seq_along(simulation@specs), function(i){ifelse(i %in% names(occ),occ[paste(i)], 0)})) 
+})
+mat_bac  <- do.call(cbind, list)
+rownames(mat_bac) <- names(simulation@specs)
+
+write.csv2(mat_bac, file = "output.csv", row.names = TRUE)
+
 
 #ANALIZA WYNIKÓW,...
 par(mfrow=c(1,2))
 plotCurves2(simulation, legendpos = "bottomleft")
 
-#par(mfrow=c(2,5))
-#evalArena(simulation, show_legend = FALSE, time=1:10)
-
-
-#WYJŚCIE
-#varSubs <- getVarSubs(simulation)
-#getSubHist(simulation, "EX_lac_L(e)")
+par(mfrow=c(2,5))
+evalArena(simulation, show_legend = FALSE, time=1:10)
 
 
 
-#diet_out <- diet
 
